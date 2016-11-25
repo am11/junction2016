be4cc3aa3f9279c9bfb344d1bf0e3c42d6b8004b
Mikko Niiranen <mikko.niiranen@nextgames.com>
2016-11-26 00:39:57 +0200

    Unity jar plugin

---
+++ Junction/Assets/JarTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JarTest : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		//AndroidJavaClass jc = new AndroidJavaClass("io.proximi.proximiiolibrary");
+		//AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("ProximiioFactory");
+		//AndroidJavaObject proxiimio = jo.Call<AndroidJavaObject>("getProximiio",this,null);
+		callShareApp();
+
+	}
+	string subject = "WORD-O-MAZE";
+	string body = "PLAY THIS AWESOME. GET IT ON THE PLAYSTORE";
+
+	public void callShareApp()
+	{
+		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+		currentActivity.Call("shareText", subject, body);
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
