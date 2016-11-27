77187eefc9ad25bd3a4739e1620a0a6fb82970c2
Adeel <adeelbm@outlook.com>
2016-11-27 07:14:27 +0200

    Add ability to get positions from server

--- Junction/Assets/Scripts/GameManager.cs
+++ Junction/Assets/Scripts/GameManager.cs
@@ -10,16 +10,23 @@ public class GameManager : MonoBehaviour
     private float ownPositionTimer;
     public float ownPositionFrequency;
     public Text DebugText;
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+    public string playerId;
+    public string targetId;
 
     // Use this for initialization
     void Start()
     {
+        playerId = "???";
+        targetId = "???";
+
         ownPositionTimer = ownPositionFrequency;
         SetTargetPlayer();
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator Update()
     {
         TargetPlayer.UpdatePosition();
         if (ownPositionTimer <= 0)
@@ -32,11 +39,54 @@ public class GameManager : MonoBehaviour
             ownPositionTimer -= Time.deltaTime;
         }
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-        DebugText.text = MapCoordinates.ToMapCoordinates(GetPosition()).ToString();
+#if UNITY_ANDROID || !UNITY_EDITOR
+        MapCoordinates coordinates = MapCoordinates.ToMapCoordinates(GetPosition());
+
+        DebugText.text = coordinates.ToString();
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime = Time.time + period;
+            yield return GetOpponentPosition(coordinates);
+        }
 #else
         DebugText.text = GetPosition();
+        yield break;
 #endif
+    }
+
+    private IEnumerator GetOpponentPosition(MapCoordinates coordinates)
+    {
+        WWW www = new WWW("https://ngjunction2016.azurewebsites.net/api/HttpTriggerJS1?code=rVbhaoGvX5L/WNnJQEmMpkPiRNrFykb7aiDBEpF0qv7W4xAE9adbQQ==&playerId=" + playerId + "&latitude=" + coordinates.Latitude + "&longitude=" + coordinates.Longitude + "&accuracy=" + coordinates.Accuracy + "&opponentId=" + targetId);
+
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+    private IEnumerator GetAllPositions(MapCoordinates coordinates)
+    {
+        WWW www = new WWW("https://ngjunction2016.azurewebsites.net/api/HttpTriggerJS1?code=rVbhaoGvX5L/WNnJQEmMpkPiRNrFykb7aiDBEpF0qv7W4xAE9adbQQ==&playerId=" + playerId + "&latitude=" + coordinates.Latitude + "&longitude=" + coordinates.Longitude + "&accuracy=" + coordinates.Accuracy);
+
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
     }
 
     public float GetDistanceBetweenPlayers(PlayerVisualizer playerA, PlayerVisualizer playerB)
